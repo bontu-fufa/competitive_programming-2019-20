a573e713e8df1e87bd8ddbb2002115af62d86b56
bontu-fufa <50772680+bontu-fufa@users.noreply.github.com>
2020-02-17 15:47:08 +0300

    Update count-negative-numbers-in-a-sorted-matrix.py

--- camp/day 12/count-negative-numbers-in-a-sorted-matrix.py
+++ camp/day 12/count-negative-numbers-in-a-sorted-matrix.py
@@ -4,11 +4,36 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         negatives  = 0
+                
+            
+            
+            
+#         O(n*m)
         if grid == [[]]: return 0
         
         for i in range(m-1,-1,-1):
             for j in range(n):
                 if grid[i][j] < 0:
+                
                     negatives += n - j
                     break
         return negatives
+        
+        
+        
+        
+        
+#         O(n+m)    
+        row = 0
+        column = n-1
+
+        while 0 <= row < m and 0 <= column < n :
+            
+            if grid[row][column] < 0:
+                negatives += m - row
+                column -= 1
+            else: 
+                row += 1
+                
+        return negatives
+
