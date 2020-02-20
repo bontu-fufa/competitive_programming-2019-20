890cfed1c38b0fd30b61a6fcc23b7a5fb87eb9a1
bontu-fufa <50772680+bontu-fufa@users.noreply.github.com>
2020-02-20 09:25:05 +0300

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.py

--- day26/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ day26/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,13 +1,18 @@
 #https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        if len(prices) == 0: return 0
         
-        buy = - prices[0]
+        if len(price) == 1: return 0
+        
+        buy = prices[0]
         profit = 0
-        
         for i in range(1,len(prices)):
-            profit = max(profit,buy+prices[i] - fee)
-            buy = max(buy,profit-prices[i])
+            if prices[i] > buy + fee:
+                profit += prices[i] - buy - fee
+                buy = prices[i] - fee
+            if prices[i] < buy:
+                buy = prices[i]
+                
+                
         return profit
     
