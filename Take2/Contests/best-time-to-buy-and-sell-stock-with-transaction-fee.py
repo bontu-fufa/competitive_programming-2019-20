5de261be6cca491ace8a2522b8237fde5a943188
bontu-fufa <50772680+bontu-fufa@users.noreply.github.com>
2020-12-02 15:42:44 +0300

    Create best-time-to-buy-and-sell-stock-with-transaction-fee.py

---
+++ Take2/Contests/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        if len(prices) < 2: return 0
+        profit = 0
+        buy_price = curr_max = prices[0]
+        for price in prices:
+            if curr_max - buy_price > fee and price < curr_max - fee:
+                # print("is profit" ,price)
+                profit += curr_max - buy_price - fee
+                buy_price = price
+                curr_max = price
+            elif curr_max - buy_price <= fee and price < buy_price:
+                # print("update buy - >less" ,price)
+
+                buy_price = price
+                curr_max = price
+            elif price > curr_max:
+                # print("update max price" ,price)
+
+                curr_max = price
+        if curr_max - buy_price > fee:
+            print(price)
+            profit += curr_max - buy_price - fee
+        return profit
