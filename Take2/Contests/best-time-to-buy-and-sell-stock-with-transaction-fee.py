08b967587e754c60ba0e83f1e1670e133b7fb8d8
bontu-fufa <50772680+bontu-fufa@users.noreply.github.com>
2020-12-02 15:46:42 +0300

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.py

--- Take2/Contests/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ Take2/Contests/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -5,7 +5,7 @@ class Solution:
         buy_price = curr_max = prices[0]
         for price in prices:
             if curr_max - buy_price > fee and price < curr_max - fee:
-                # print("is profit" ,price)
+                # print("is profit , price is smaller" ,price)
                 profit += curr_max - buy_price - fee
                 buy_price = price
                 curr_max = price
@@ -21,4 +21,21 @@ class Solution:
         if curr_max - buy_price > fee:
             print(price)
             profit += curr_max - buy_price - fee
+        return profit
+
+    
+#     another
+    class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        if len(prices) == 1: return 0
+        
+        buy = prices[0]
+        profit = 0
+        
+        for i in range(1,len(prices)):
+            if prices[i] > buy + fee:
+                profit += prices[i] - buy - fee
+                buy = prices[i] - fee
+            if prices[i] < buy:
+                buy = prices[i]
         return profit
